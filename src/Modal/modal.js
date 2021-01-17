229c3d8a3e767127742a489772516e8c90b56487
OlegEf <5tg6yy@gmail.com>
2021-01-17 20:05:12 +0300

    reactjs modal-window 1commmit

---
+++ src/Modal/modal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './modal.css';
+
+	const ButtonMmodal = () => {
+	  return <button className="modal-react">vb</button>
+	}
+
+export default ButtonMmodal;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app';
+import ButtonMmodal from './Modal';
+
+const Modal = () => {
+  return (
+		<div className="modal">
+			modal
+		</div>
+  );
+}
+
+const App = () => {
+
+  return (
+    <div>
+      <Modal />
+      <ButtonMmodal />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />,
   document.getElementById('root'));
